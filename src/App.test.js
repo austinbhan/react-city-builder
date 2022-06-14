9bb05ddd98dea5c79b789264a85b6f0d7d26f5e6
austinbhan <austinbhan@outlook.com>
2022-06-13 17:43:15 -0700

    to as test parameter

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/slogan/i);
+  const linkElement = screen.getByText(/to/i);
   expect(linkElement).toBeInTheDocument();
 });
